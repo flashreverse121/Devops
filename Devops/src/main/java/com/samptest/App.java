b0b1ad1bd053346212bbc1180202d97f4dc24778
flashreverse121 <52461617+flashreverse121@users.noreply.github.com>
2020-12-26 11:22:38 +0530

    Update App.java

--- Devops/src/main/java/com/samptest/App.java
+++ Devops/src/main/java/com/samptest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! fisr application" );
+        System.out.println( "Hello World! Maven+Docker Integration" );
     }
 }
