ea64ffc80526cdc4e2952921743b818664c38ec0
flashreverse121 <52461617+flashreverse121@users.noreply.github.com>
2020-12-31 23:52:45 +0530

    Add files via upload
    Testing War genration

---
+++ tomcatApacheWar/src/main/java/com/example/tomcatApacheWar/TomcatApacheWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcatApacheWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatApacheWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatApacheWarApplication.class, args);
+	}
+
+}
---
+++ tomcatApacheWar/src/test/java/com/example/tomcatApacheWar/TomcatApacheWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomcatApacheWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatApacheWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
